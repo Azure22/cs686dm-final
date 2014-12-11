87ca0660c33a53af913a4b41a8998a635d8f4870
Davincode <luchao@LuChaos-MacBook-Pro.local>
2014-12-11 01:23:05 -0800

    final
    util is a tool class, don’t forget about it

--- Naive.py
+++ Naive.py
@@ -1,27 +1,7 @@
-from sklearn import svm
-from sklearn import linear_model
 import util
 import math
-import csv
-
-
-class Multi():
-
-    def __init__(self, num_features=10):
-        self.num_bags = num_features
-        self.svm = svm.SVC()
-        self.max_ent = linear_model.LogisticRegression
-        self.Naive_Bayes = NaiveBayesClassifier(self.num_bags)
-
-    def train(self, X, Y):
-        self.svm.fit(X, Y)
-        self.max_ent.fit(X, Y)
-
-    def predict_svm(self, X, Y):
-        return self.svm.score(X, Y)
-
-    def predict_max(self, X, Y):
-        return self.max_ent.score(X, Y)
+import numpy as np
+import pandas as pd
 
 
 class NaiveBayesClassifier():
@@ -72,8 +52,8 @@ class NaiveBayesClassifier():
             else:
                 self.temp_1[label] = 0
 
-            for label in self.legalLabels:
-                self.temp_2[label] = {}
+        for label in self.legalLabels:
+            self.temp_2[label] = {}
             for bag in self.features:
                 self.temp_2[label][bag] = {}
                 for value in range(2):
@@ -92,43 +72,55 @@ class NaiveBayesClassifier():
         return guesses
 
     def calculateLogJointProbabilities(self, review):
-        logJoint = util.Counter()
 
+        logJoint = util.Counter()
         for label in self.legalLabels:
             p = math.log(self.temp_1[label])
             q = 0
             for bag in range(len(review)):
                 value = review[bag]
-                q += math.log(self.temp_2[label][bag][value])
+                if label in self.temp_2:
+                    if bag in self.temp_2[label]:
+                        if value in self.temp_2[label][bag]:
+                            q += math.log(self.temp_2[label][bag][value])
             logJoint[label] = p + q
 
         return logJoint
 
 
-if __name__ == '__main__':
+def interface(filename1, filename2):
     train_data = []
     train_labels = []
     test_data = []
     test_labels = []
 
-    f_train = open("train_data_b.csv", "r")
-    for line in csv.reader(f_train.readline().splitlines()):
-        train_data.append(line[0:-1])
-        train_labels.append(line[-1])
+    trainList = np.array(pd.read_csv(filename1).values.tolist(), dtype=np.int_).tolist()
+    testList = np.array(pd.read_csv(filename2).values.tolist(), dtype=np.int_).tolist()
 
-    f_test = open("test_data_b.csv", "r")
-    for line in csv.reader(f_test.readline().splitlines()):
-        test_data.append(line[0:-1])
-        test_labels.append(line[-1])
+    a = None
+    for line in trainList:
+        if a is None:
+            a = 1
+        else:
+            train_data.append(line[0:-1])
+            train_labels.append(line[-1])
+
+    b = None
+    for line in testList:
+        if b is None:
+            b = 1
+        else:
+            test_data.append(line[0:-1])
+            test_labels.append(line[-1])
 
     if len(train_data):
-        multi = Multi(len(train_data[0]))
-        multi.Naive_Bayes.train(train_data, train_labels)
-        guesses = multi.Naive_Bayes.classify(test_data)
+        clf = NaiveBayesClassifier(len(train_data[0]))
+        clf.train(train_data, train_labels)
+        guesses = clf.classify(test_data)
 
         correct = 0
         for i in range(len(test_labels)):
             if test_labels[i] == guesses[i]:
                 correct += 1
-        print "Naive Bayes: "
-        print float(correct) / len(test_labels)
+        accuracy = float(correct) / len(test_labels)
+        return accuracy
